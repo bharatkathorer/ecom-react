6370fd846500878d6b915fae3f25df97bdb49377
Bharat Kathore <49843578+bharatkathorer@users.noreply.github.com>
2025-02-12 20:34:19 +0530

    build issue fixed

--- src/routes/backend/BackendRoutes.tsx
+++ src/routes/backend/BackendRoutes.tsx
@@ -1,6 +1,5 @@
 import {Route, Routes} from "react-router-dom";
 import LoginPage from "../../pages/backend/auth/LoginPage.tsx";
-import RegisterPage from "../../pages/backend/auth/RegisterPage.tsx";
 import PrivateRoute from "./PrivateRoute.tsx";
 import DashboardPage from "../../pages/backend/DashboardPage.tsx";
 import NotFound from "../../pages/errors/NotFound.tsx";
@@ -11,7 +10,6 @@ const FrontEndRoutes = () => {
         <Routes>
             {/* Public Routes */}
             <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/register" element={<RegisterPage/>}/>
 
             {/* Private Routes */}
             <Route element={<PrivateRoute/>}>
