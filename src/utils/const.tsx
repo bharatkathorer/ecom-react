cfa0c24447bf494389621bd7d8a907e23979b51b
Bharat Kathore <49843578+bharatkathorer@users.noreply.github.com>
2025-02-14 12:05:54 +0530

    Update const.tsx

--- src/utils/const.tsx
+++ src/utils/const.tsx
@@ -1,8 +1,8 @@
 import {setLogin, setToken, setUser} from "../store/slice/authSlice.ts";
 import {setAdmin, setAdminLogin, setAdminToken} from "../store/slice/adminAuthSlice.ts";
 
-// export const BASE_URL: string = 'https://ecommerce-vibx.onrender.com';
-export const BASE_URL: string = 'https://ecom-node-js.vercel.app';
+export const BASE_URL: string = 'https://ecommerce-vibx.onrender.com';
+// export const BASE_URL: string = 'https://ecom-node-js.vercel.app';
 // export const BASE_URL: string = ' http://localhost:8000';
 
 export const makeUrl = (url: string): string => {
